f470911a77d912235843d0505c413a35484e8fa9
sahargholami1992 <sahar_gholami03@gmail.com>
2023-12-19 12:49:40 +0330

    modify main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.example.entity.Student;
+import org.example.menu.MainMenu;
+import org.example.menu.StudentMenu;
+import org.example.service.ProcessPaymentService;
+import org.example.service.StudentService;
+import org.example.utill.ApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        StudentMenu studentMenu = new StudentMenu();
+//        studentMenu.runMenu();
+        MainMenu menu = new MainMenu();
+        menu.mainMenu();
+//        ProcessPaymentService service = ApplicationContext.getProcessPaymentService();
+
     }
 }
