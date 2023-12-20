ebcc9fb86010f55d8688e5e0d6f80a856647598f
sahargholami1992 <sahar_gholami03@gmail.com>
2023-12-20 15:08:48 +0330

    rename main to Application

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+import org.example.menu.MainMenu;
+
+public class Application {
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+        menu.mainMenu();
+    }
+}
